c966336ca04f0fabfef743c138a8fc2b2390dbcb
Tom Fleet <me@followtheprocess.codes>
2026-08-11 09:08:57 +0100

    Make nix fmt run golangci-lint

--- nix/treefmt.nix
+++ nix/treefmt.nix
@@ -1,26 +1,39 @@
 _: {
-  perSystem.treefmt.programs = {
-    deadnix.enable = true;
-    gofmt.enable = true;
-    nixfmt.enable = true;
-    shellcheck.enable = true;
-    shfmt.enable = true;
-    statix.enable = true;
-    sqlfluff = {
-      enable = true;
-      dialect = "sqlite";
-    };
-    yamlfmt = {
-      enable = true;
-      settings.formatter = {
-        type = "basic";
-        eof_newline = true;
-        indent = 2;
-        pad_line_comments = 2;
-        retain_line_breaks_single = true;
-        scan_folded_as_literal = true;
-        trim_trailing_whitespace = true;
+  perSystem =
+    { pkgs, ... }:
+    {
+      treefmt = {
+        programs = {
+          deadnix.enable = true;
+          nixfmt.enable = true;
+          shellcheck.enable = true;
+          shfmt.enable = true;
+          statix.enable = true;
+          sqlfluff = {
+            enable = true;
+            dialect = "sqlite";
+          };
+          typos.enable = true;
+          yamlfmt = {
+            enable = true;
+            settings.formatter = {
+              type = "basic";
+              eof_newline = true;
+              indent = 2;
+              pad_line_comments = 2;
+              retain_line_breaks_single = true;
+              scan_folded_as_literal = true;
+              trim_trailing_whitespace = true;
+            };
+          };
+        };
+
+        # Use golangci-lint fmt as a Go formatter
+        settings.formatter.golangci-lint-fmt = {
+          command = "${pkgs.golangci-lint}/bin/golangci-lint";
+          options = [ "fmt" ];
+          includes = [ "*.go" ];
+        };
       };
     };
-  };
 }
